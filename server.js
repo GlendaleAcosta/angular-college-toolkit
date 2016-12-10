1dfbf102ac51d12ad2192d2c4520b59e32a2401b
Glendale Acosta <gglendale17@gmail.com>
2016-12-10 12:28:46 -0800

    Fixed minor validation issue with login.

--- server.js
+++ server.js
@@ -152,6 +152,8 @@ app.post('/login', validator(), function(req, res){
 
                          });
 
+                     } else {
+                         res.send({msg: "Password or email is incorrect."});
                      }
                 })
        }
